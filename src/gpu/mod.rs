0ee53bd1830d088f15888ba8ccf4de0c874fc69f
gamemake-eng <mohinathsivaneswaran@gmail.com>
2024-07-31 12:48:50 -0400

    set up graphics

--- src/gpu/mod.rs
+++ src/gpu/mod.rs
@@ -2,15 +2,29 @@ pub struct Gpu {
     pub frontbuf: Vec<u32>,
     pub backbuf: Vec<u32>,
     pub texbuf: Vec<u32>,
-    pub regs: Vec<f32>
+    pub regs: Vec<i32>
 }
 
-/*trait Gpu {
-    fn new() -> Gpu;
-    fn blit(&self);
-    fn clear(&self);
-    fn flip(&self);
-}*/
+/*
+ * regs
+#define DX 0
+#define DY 1
+#define SX 2
+#define SY 3
+#define W 4
+#define H 5
+#define CC 6
+*/
+
+pub enum GPU_CMD {
+    DX = 0,
+    DY = 1,
+    SX = 2,
+    SY = 3,
+    W  = 4,
+    H  = 5,
+    CC = 6
+}
 
 impl Gpu {
     pub fn new() -> Gpu {
@@ -18,17 +32,30 @@ impl Gpu {
             frontbuf: vec![0u32; 320*240],
             backbuf: vec![0u32; 320*240],
             texbuf: vec![0u32; 1024*1024],
-            regs: vec![0f32; 7]
+            regs: vec![0i32; 7]
 
         }
     }
-    pub fn blit(&self){
-        println!("Not implmented");
+    pub fn blit(&mut self){
+        
     }
-    pub fn clear(&self){
-        println!("Not implmented");
+    pub fn clear(&mut self){
+        //println!("Not implmented");
+        for pixel in self.backbuf.iter_mut() {
+            *pixel = self.regs[GPU_CMD::CC as usize] as u32;
+        }
     }
-    pub fn flip(&self){
-        println!("Not implmented");
+    pub fn flip(&mut self){
+        for (index, pixel) in self.backbuf.iter_mut().enumerate(){
+            //println!("index {index} pixel {pixel}");
+            self.frontbuf[index] = *pixel;
+        }
+
+        /*for (index, pixel) in self.frontbuf.iter_mut().enumerate(){     
+            println!("index {index} pixel {pixel}");
+        }*/ 
+
+
+
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,48 @@
 use wasmer::{Store, Module, Instance, Value, imports};
+use minifb::{Key, Window, WindowOptions};
 
 mod gpu;
 
 use gpu::Gpu;
 
 fn main() -> anyhow::Result<()> {
-    
-    let _gpu = Gpu::new();
+	
+	let mut gpu = Gpu::new();
 
-    let module_wat = r#"
-    (module
-    (type $t0 (func (param i32) (result i32)))
-    (func $add_one (export "add_one") (type $t0) (param $p0 i32) (result i32)
-        get_local $p0
-        i32.const 1
-        i32.add))
-    "#;
+	let module_wat = r#"
+	(module
+	(type $t0 (func (param i32) (result i32)))
+	(func $add_one (export "add_one") (type $t0) (param $p0 i32) (result i32)
+			get_local $p0
+			i32.const 1
+			i32.add))
+	"#;
 
-    let mut store = Store::default();
-    let module = Module::new(&store, &module_wat)?;
-    // The module doesn't import anything, so we create an empty import object.
-    let import_object = imports! {};
-    let instance = Instance::new(&mut store, &module, &import_object)?;
-
-    let add_one = instance.exports.get_function("add_one")?;
-    let result = add_one.call(&mut store, &[Value::I32(42)])?;
-    println!("{:?}",result[0]);
+	let mut store = Store::default();
+	let module = Module::new(&store, &module_wat)?;
+	// The module doesn't import anything, so we create an empty import object.
+	let import_object = imports! {};
+	let instance = Instance::new(&mut store, &module, &import_object)?;
+	
+        
 
 
+	let mut window = Window::new(
+	    "Test",
+	    320,
+	    240,
+	    WindowOptions::default()
+	)?;
+	window.set_target_fps(60);
+	
+	while window.is_open() && !window.is_key_down(Key::Escape) {
+	    gpu.regs[6] = 0xff00fff0u32 as i32;
+            gpu.clear();
+	    gpu.flip();
+	    window.update_with_buffer(&gpu.frontbuf, 320, 240)?;
+    	}
 
 
-    Ok(())
+
+    	Ok(())
 }
