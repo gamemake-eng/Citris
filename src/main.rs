66adffee2350a3e2793c91676bd57aaeda7f8045
gamemake-eng <mohinathsivaneswaran@gmail.com>
2024-08-02 13:13:49 -0400

    last commit

--- src/main.rs
+++ src/main.rs
@@ -11,21 +11,48 @@ mod gpu;
 
 use gpu::{Gpu, gpu_reg};
 
-struct Env {
-    gpu: Gpu,
+struct Env<'a> {
+    gpu:  &'a mut Gpu,
     wasi: WasiCtx
 }
 
 fn main() -> anyhow::Result<()> {
     //let args: Vec<String> = env::args().collect();    
-
+    
     let mut gpu = Gpu::new();
-
 
     let wasm_path = concat!(
         env!("CARGO_MANIFEST_DIR"),
         "/template/c/build/game.wasm"
     );
+
+    let engine = Engine::default();
+    let mut linker = Linker::new(&engine);
+    
+    linker.func_wrap("env", "gpu_cmd", |caller: Caller<'_, Env>, cmd: u32| {
+        caller.data().gpu.cmd(cmd);
+    })?;
+
+    wasi_common::sync::add_to_linker(&mut linker, |s: &mut Env| &mut s.wasi)?;
+
+    let wasi = WasiCtxBuilder::new()
+        .inherit_stdio()
+        .inherit_args()?
+        .build();
+
+    let  env = Env {
+        gpu: &mut gpu,
+        wasi: wasi
+    };
+
+    let mut store = Store::new(&engine, env);
+
+    let module = Module::from_file(&engine, wasm_path)?;
+    linker.module(&mut store, "", &module)?;
+    linker
+        .get_default(&mut store, "")?
+        .typed::<(), ()>(&store)?
+        .call(&mut store, ())?;
 
 
     let mut window = Window::new(
@@ -37,21 +64,6 @@ fn main() -> anyhow::Result<()> {
     window.set_target_fps(60);
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        /*gpu.regs[6] = 0xff00fff0u32 as i32;
-        gpu.clear();
-        gpu.regs[gpu_reg::W] = 32;
-        gpu.regs[gpu_reg::H] = 32;
-        gpu.regs[gpu_reg::DX] = 10;
-        gpu.regs[gpu_reg::DY] = 10;
-        gpu.blit();
-
-        gpu.regs[gpu_reg::W] = 32;
-        gpu.regs[gpu_reg::H] = 32;
-        gpu.regs[gpu_reg::DX] = 25;
-        gpu.regs[gpu_reg::DY] = 25;
-        gpu.blit();
-
-        gpu.flip();*/
 
         //x+=1;
 
